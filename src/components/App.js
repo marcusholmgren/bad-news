625be1c08991d2191f5da2b7fb8d1b1478221bce
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-16 21:58:02 +0200

    feat: add header with navigation links

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,35 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import CreateLink from "./Link/CreateLink";
 import Login from "./Auth/Login";
 import ForgotPassword from "./Auth/ForgotPassword";
 import SearchLinks from "./Link/SearchLink";
 import LinkList from "./Link/LinkList";
 import LinkDetail from "./Link/LinkDetail";
+import Header from "./Header";
 
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LinkList page="1" />} />
-            <Route path="/create" element={<CreateLink />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot" element={<ForgotPassword />} />
-            <Route path="/search" element={<SearchLinks />} />
-            <Route path="/top" element={<LinkList />} />
-            <Route path="/new/:page" element={<LinkList />} />
-            <Route path="/link/:linkId" element={<LinkDetail />} />
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <Header/>
+                <div className="route-container">
+                    <Routes>
+                        <Route path="/" element={<LinkList page="1"/>}/>
+                        <Route path="/create" element={<CreateLink/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/forgot" element={<ForgotPassword/>}/>
+                        <Route path="/search" element={<SearchLinks/>}/>
+                        <Route path="/top" element={<LinkList/>}/>
+                        <Route path="/new/:page" element={<LinkList/>}/>
+                        <Route path="/link/:linkId" element={<LinkDetail/>}/>
+                    </Routes>
+
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;

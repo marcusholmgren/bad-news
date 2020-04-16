8d8c96bb894fffb57c13a22807ee5304965afd1c
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-16 21:39:50 +0200

    feat: add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import CreateLink from "./Link/CreateLink";
+import Login from "./Auth/Login";
+import ForgotPassword from "./Auth/ForgotPassword";
+import SearchLinks from "./Link/SearchLink";
+import LinkList from "./Link/LinkList";
+import LinkDetail from "./Link/LinkDetail";
+
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo512.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LinkList page="1" />} />
+            <Route path="/create" element={<CreateLink />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot" element={<ForgotPassword />} />
+            <Route path="/search" element={<SearchLinks />} />
+            <Route path="/top" element={<LinkList />} />
+            <Route path="/new/:page" element={<LinkList />} />
+            <Route path="/link/:linkId" element={<LinkDetail />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 

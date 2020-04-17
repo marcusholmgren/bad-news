98d65442645a3cdb01c42e01d3f1c00560980299
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-17 22:16:07 +0200

    feat: create custom form hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,38 @@
 import React, { useState} from "react";
+import useFormValidation from './useFormValidation';
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
+  const { handleSubmit, handleChange, values} = useFormValidation(INITIAL_STATE);
   const [login, setLogin] = useState(true);
 
   return <div>
     <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-    <form action="" className="flex flex-column">
-      {!login && <input type="text" placeholder="Your name" autoComplete="off" />}
-      <input type="email" placeholder="Your email" autoComplete="off" />
-      <input type="password" placeholder="Choose a secure password"/>
+    <form onSubmit={handleSubmit} className="flex flex-column">
+      {!login && <input
+          type="text"
+          name="name"
+          value={values.name}
+          placeholder="Your name"
+          autoComplete="off"
+          onChange={handleChange} />
+      }
+      <input type="email"
+             name="email"
+             value={values.email}
+             placeholder="Your email"
+             autoComplete="off"
+             onChange={handleChange} />
+      <input type="password"
+             name="password"
+             value={values.password}
+             placeholder="Choose a secure password"
+             onChange={handleChange}/>
       <div className="flex mt3">
         <button type="submit" className="button pointer mr2">
           Submit

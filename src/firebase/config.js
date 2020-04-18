ac16585a76aebfb7d498e6e647788947506728d6
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-18 21:06:48 +0200

    fix: add empty Firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-cloud-firestore-project>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-sender-id>'
+};
+
+export default firebaseConfig;

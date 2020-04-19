c6f09d4ed33c736aca0c541865fc3792ed9b4864
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-19 14:17:00 +0200

    feat: display links

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,36 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {getDomain} from "../../utils";
 
-function LinkItem() {
-  return <div>LinkItem</div>;
+
+function LinkItem({ link, index, showCount }) {
+    const option = {hour: "numeric", dayPeriod: "short"};
+    const dateTimeFormat = new Intl.DateTimeFormat("en-US", option);
+    return (
+      <div className="flex items-start mt2">
+        <div className="flex items-center">
+          {showCount && <span className="gray">{index}</span>}
+          <div className="vote-button">
+            &#x25B2;
+          </div>
+          <div className="ml1">
+              <div>
+                {link.description} <span className="link">({getDomain(link.url)})</span>
+              </div>
+              <div className="f6 lh-copy gray">
+                  {link.votes.length} votes by {link.postedBy.name} {dateTimeFormat.format(link.created)}
+                  {" | "}
+                  <Link to={`/link/${link.id}`}>
+                      {link.comments.length > 0
+                          ? `${link.comments.length} comments`
+                          : "discuss"
+                      }
+                  </Link>
+              </div>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default LinkItem;
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,9 +1,11 @@
-import React, {useCallback, useContext, useEffect} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import FirebaseContext from "../../firebase/context";
+import LinkItem from "./LinkItem";
 
 function LinkList() {
     const {firebase} = useContext(FirebaseContext);
     const getLinksOnce = useCallback(getLinks, []);
+    const [links, setLinks] = useState([]);
 
     useEffect(() => {
         getLinksOnce();
@@ -17,14 +19,25 @@ function LinkList() {
             }
         })
 
-        console.log(links);
+        setLinks(links);
     }
 
     function getLinks() {
         firebase.db.collection("links").onSnapshot(handleSnapshot)
     }
 
-    return <div>LinkList</div>;
+    return (
+        <div>
+            {links.map((link, index) => (
+                <LinkItem
+                    key={link.id}
+                    showCount={true}
+                    link={link}
+                    index={index + 1}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default LinkList;

6a618efbab0ccdea6ad8f4a92b5bd52080054828
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-19 12:18:54 +0200

    feat: get links from Firestore

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
+import FirebaseContext from "../../firebase/context";
 
-function LinkList(props) {
+function LinkList() {
+  const { firebase } = useContext(FirebaseContext);
+  const getLinksOnce = useCallback(() => getLinks(), []);
+
+  useEffect(() => {
+    getLinksOnce();
+  }, [getLinksOnce]);
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+
+    console.log(links);
+  }
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot)
+  }
+
   return <div>LinkList</div>;
 }
 

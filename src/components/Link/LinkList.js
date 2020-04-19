8c7592c6b0b8f093cf5e8a49fc0571cb7b571da1
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-04-19 12:30:32 +0200

    fix: build error with missing function

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,30 +1,30 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, {useCallback, useContext, useEffect} from "react";
 import FirebaseContext from "../../firebase/context";
 
 function LinkList() {
-  const { firebase } = useContext(FirebaseContext);
-  const getLinksOnce = useCallback(() => getLinks(), []);
+    const {firebase} = useContext(FirebaseContext);
+    const getLinksOnce = useCallback(getLinks, []);
 
-  useEffect(() => {
-    getLinksOnce();
-  }, [getLinksOnce]);
+    useEffect(() => {
+        getLinksOnce();
+    }, [getLinksOnce]);
 
-  function handleSnapshot(snapshot) {
-    const links = snapshot.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data()
-      }
-    })
+    function handleSnapshot(snapshot) {
+        const links = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        })
 
-    console.log(links);
-  }
+        console.log(links);
+    }
 
-  function getLinks() {
-    firebase.db.collection("links").onSnapshot(handleSnapshot)
-  }
+    function getLinks() {
+        firebase.db.collection("links").onSnapshot(handleSnapshot)
+    }
 
-  return <div>LinkList</div>;
+    return <div>LinkList</div>;
 }
 
 export default LinkList;
